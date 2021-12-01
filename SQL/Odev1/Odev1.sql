f17768a02ff38c42e9c7fe11466af42759195e2d
Mustafa Selim Gunaydin <mustafaselimgunaydin@gmail.com>
2021-12-01 19:55:10 +0300

    Create Odev1.sql

---
+++ SQL/Odev1/Odev1.sql
@@ -0,0 +1,18 @@
+--Task1
+SELECT title,description FROM film;
+
+--Task2
+SELECT * FROM film
+WHERE length > 60 AND length < 75;
+
+--Task3
+SELECT * FROM film
+WHERE rental_rate = 0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99;
+
+--Task4
+SELECT last_name FROM customer
+WHERE first_name = 'Mary';
+
+--Task5
+SELECT * FROM film
+WHERE NOT ( length > 50 AND rental_rate = 2.99 OR rental_rate = 4.99 );
