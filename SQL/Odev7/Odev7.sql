e867791fe11ef956ae6999bcb269eb87eaa246f9
Mustafa Selim Gunaydin <mustafaselimgunaydin@gmail.com>
2021-12-03 00:13:13 +0300

    Update Odev7.sql

--- SQL/Odev7/Odev7.sql
+++ SQL/Odev7/Odev7.sql
@@ -9,3 +9,11 @@ GROUP BY replacement_cost
 HAVING COUNT(*) > 50
 ORDER BY COUNT(*) DESC;
 
+--Task3
+SELECT store_id,COUNT(*) FROM customer
+GROUP BY store_id;
+
+--Task4
+SELECT country_id,COUNT(*) FROM city
+GROUP BY country_id
+HAVING COUNT(*) > 55;
